4aee31fd6e88439568a6b907e26f371e5bac12b4
Jonathan Song <jonsong2013@yahoo.com>
2023-12-14 16:32:23 -0500

    Pipeline test

--- test.py
+++ test.py
@@ -1,5 +1,25 @@
 import datasets
+from transformers import EncoderDecoderModel, BertTokenizer
 
-train_data = datasets.load_dataset("cnn_dailymail", "3.0.0", split="train")
+test_data = datasets.load_dataset("cnn_dailymail", "3.0.0", split="test[:16]")
+rouge = datasets.load_metric("rouge")
 
-print(train_data)
+bert2bert = EncoderDecoderModel.from_pretrained("patrickvonplaten/bert2bert_cnn_daily_mail").to("cuda")
+tokenizer = BertTokenizer.from_pretrained("patrickvonplaten/bert2bert_cnn_daily_mail")
+
+def generate_summary(batch):
+    # cut off at BERT max length 512
+    inputs = tokenizer(batch["article"], padding="max_length", truncation=True, max_length=512, return_tensors="pt")
+    input_ids = inputs.input_ids.to("cuda")
+    attention_mask = inputs.attention_mask.to("cuda")
+
+    outputs = bert2bert.generate(input_ids, attention_mask=attention_mask)
+
+    output_str = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+    batch["pred_summary"] = output_str
+
+    return batch
+
+results = test_data.map(generate_summary, batched=True, batch_size=16, remove_columns=["article"])
+print(rouge.compute(predictions=results["pred_summary"], references=results["highlights"], rouge_types=["rouge1", "rouge2", "rougeL"]))
